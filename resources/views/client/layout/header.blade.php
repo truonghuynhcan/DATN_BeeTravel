7adce55e435e5586283fa1f30ce318c558710d95
truonghuynhcan <truonghuynhcan@gmail.com>
2024-10-10 22:11:36 +0700

    [style] chỉnh header phần đăng nhập đăng ký

--- resources/views/client/layout/header.blade.php
+++ resources/views/client/layout/header.blade.php
@@ -1,12 +1,14 @@
 <style>
-    #nav2 .nav-link.active{
+    #nav2 .nav-link.active {
         color: var(--bs-primary);
         font-weight: bold;
     }
-    #nav2 .nav-link{
+
+    #nav2 .nav-link {
         transition: color 0.4s ease, font-weight 0.3s ease, text-decoration 0.3s ease;
     }
-    #nav2 .nav-link:hover{
+
+    #nav2 .nav-link:hover {
         font-weight: bold;
         text-decoration: underline;
         color: var(--bs-primary);
@@ -16,30 +18,33 @@
     <!-- nav 1 -->
     <nav class="navbar navbar-expand-lg p-0">
         <div class="container-fluid  bg-secondary bg-opacity-50">
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="container collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link text-body active " aria-current="page" href="#"> <i class="bi bi-envelope"></i> info@beechoice.net</a>
+                        <a class="nav-link text-body active " aria-current="page" href="{{route('contact')}}"> <i class="bi bi-envelope"></i> info@beechoice.net</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-body " href="#"><i class="bi bi-telephone-inbound-fill"></i>
+                        <a class="nav-link text-body " href="{{route('contact')}}"><i class="bi bi-telephone-inbound-fill"></i>
                             1900.1900.1900</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link text-body " href="#"><i class="bi bi-geo-alt-fill"></i> Chọn điểm khởi
-                            hành</a>
+                        <a class="nav-link text-body " href=""><i class="bi bi-facebook"></i></a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link text-body dropdown-toggle " href="{{ route('login') }}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bi bi-box-arrow-in-right"></i> Đăng nhập
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{ route('register') }}">Đăng Ký</a></li>
-                            <li><a class="dropdown-item" href="{{ route('login') }}">Đăng Nhập</a></li>
-
-                        </ul>
+                    <li class="nav-item">
+                        <a class="nav-link text-body " href=""><i class="bi bi-instagram"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-body " href=""><i class="bi bi-youtube"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-body " href=""><i class="bi bi-tiktok"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-body " href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                          </svg></a>
                     </li>
                 </ul>
             </div>
@@ -52,7 +57,7 @@
                 <img src="{{ asset('') }}assets/image/logo_ngang.png" alt="logo" height="60px">
             </a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0 ms-auto h5">
+                <ul class="navbar-nav mb-2 mb-lg-0 h5">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Trang chủ</a>
                     </li>
@@ -74,21 +79,40 @@
                     <li class="nav-item">
                         {{-- <a class="nav-link" href="{{ route('tour_chi_tiet') }}">Tour desctiption</a> --}}
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Tài khoản
-                        </a>
-                        <ul class="dropdown-menu" style="left: -45px;">
-                            <li><a class="dropdown-item" href="{{route('myProfile')}}">Thông tin cá nhân</a></li>
-                            <li><a class="dropdown-item" href="{{route('myTour')}}">Tour của tôi</a></li>
+
+                </ul>
+            </div>
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3" id="nav3-ul">
+                <!-- Dynamic content will be copied here from nav2 -->
+                <li class="nav-item dropdown">
+                    <a class="dropdown-toggle btn btn-primary" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Tài khoản
+                    </a>
+                    <ul class="dropdown-menu" style="left: -100px;">
+
+                        {{-- khi người dùng chưa đăng nhập thì hiện như này --}}
+                        @guest
+                            <li class="p-2">Đăng nhập hoặc đăng ký để tận dụng ưu đãi</li>
+                            <li class="p-2"><a class="btn btn-primary" href="{{ route('login') }}">Đăng nhập thành viên</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li class="p-2">Đăng nhập đại lý</li>
+                            <li class="p-2"><a class="container-fluid btn btn-outline-primary" href="">Đăng nhập đại lý</a></li>
+                        @endguest
+
+                        {{-- khi người dùng đã đăng nhập thì hiện --}}
+                        @auth
+                            <li><a class="dropdown-item" href="{{ route('myProfile') }}">Thông tin cá nhân</a></li>
+                            <li><a class="dropdown-item" href="{{ route('myTour') }}">Tour của tôi</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
                             <li><a class="dropdown-item" href="#">Đăng xuất</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
+                        @endauth
+                    </ul>
+                </li>
+            </ul>
         </div>
     </nav>
 
