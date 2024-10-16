a475f838752e98a722b73b3bf080a45c55f44759
truonghuynhcan <truonghuynhcan@gmail.com>
2024-10-16 10:53:07 +0700

    [feat] sửa vị trí cấu trức và tạo AdminLoginController

---
+++ app/Http/Controllers/AdminLoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminLoginController extends Controller
+{
+    function login_dl(Request $req)
+    {
+        $req->validate([
+            'name' => 'required|name',
+            'email' => 'required|email',
+            'password' => 'required',
+            'phone' => 'required|phone',
+        ], [
+            'name.required' => 'Vui lòng nhập tên đại lý doanh nghiệp',
+            'email.required' => 'Vui lòng nhập email',
+            'email.email' => 'Vui lòng nhập đúng định dạng email',
+            'password.required' => 'Vui lòng nhập password',
+            'phone.required' => 'Vui lòng nhập số điện thoại'
+        ]);
+        if (Auth::attempt(['name' => $req->name, 'email' => $req->email, 'password' => $req->password, 'phone' => $req->phone])) {
+            return redirect('admin');
+        } else
+            return redirect()->back()->witherrors('Sai email hoặc password đại lý');
+    }
+}
